ac915a5d76a494df3b10c6dd0d57ccdae55e021f
abbsi <nabil.abbas@gmail.com>
2020-12-26 23:50:28 +0400

    minor edits and rule updates

--- dwm/config.h
+++ dwm/config.h
@@ -7,9 +7,9 @@
 #include <X11/XF86keysym.h>
 
 /* Appearance */
-static const unsigned int borderpx = 3;              /* border pixel of windows */
+static const unsigned int borderpx = 2;              /* border pixel of windows */
 static const unsigned int snap     = 30;             /* snap pixel */
-static const unsigned int gappx    = 12;             /* pixel gap between clients */
+static const unsigned int gappx    = 30;             /* pixel gap between clients */
 static const int showbar           = 1;              /* 0 means no bar */
 static const int topbar            = 1;              /* 0 means bottom bar */
 static const int horizpadbar       = 10;             /* horizontal padding for statusbar */
@@ -46,7 +46,7 @@ static const char col_green[]     = "#009900";
 
 /* Bar opacity ; 0xff is no transparency. Play with the value to get desired transparency. */
 static const unsigned int baralpha    = 0xdd; 
-static const unsigned int borderalpha = 0xaa; // OPAQUE;
+static const unsigned int borderalpha = 0xaa; 
 static const char *colors[][3]        = {
   /*                    fg            bg            border   */
   [SchemeNorm]     = { col_offwhite,  col_bg,        col_darkgray },
@@ -103,20 +103,27 @@ static const Rule rules[] = {
    * Right = 1
    */
   /* class                        instance                       title              tags mask    swithtotag   isfloating   monitor */
-  { "Thunderbird",                "Mail",                       NULL,              2,           0,            0,           2   },
+  /* Left Monitor */
   { "firefox",                    "Navigator",                  NULL,              1,           0,            0,           2   },
-  
-  { "Code",                       "code",                       NULL,              2,           0,            0,           0   },
-  { "Sxiv",                       "sxiv",                       NULL,              128,         0,            0,           0   },
-  
-  { "zoom",                       "zoom",                       NULL,              0,           0,            1,           1   },
-  { "Slack",                      "slack",                      NULL,              2,           0,            0,           1   },
-  { "discord",                    "discord",                    NULL,              0,           0,            0,           1   },
+  { "Thunderbird",                "Mail",                       NULL,              2,           0,            0,           2   },
   { "realvnc-vncviewer",          NULL,                         NULL,              0,           0,            1,           2   },  
-  { "whatsapp-nativefier-930aa1", "whatsapp-nativefier-930aa1", NULL,              2,           0,            0,           1   },  
+  
+  /* Middle Monitor */
+  { "Code",                       "code",                       NULL,              0,           0,            0,           0   },
+  { "Sxiv",                       "sxiv",                       NULL,              0,           0,            0,           0   },
+  
+  /* Right Monitor */
+  { "Slack",                      "slack",                      NULL,              1,           0,            0,           1   },
+  { "whatsapp-nativefier-930aa1", "whatsapp-nativefier-930aa1", NULL,              1,           0,            0,           1   },  
+  { "discord",                    "discord",                    NULL,              1,           0,            0,           1   },
+  { "zoom",                       "zoom",                       NULL,              2,           0,            1,           1   },
   { NULL,                         NULL,                         "Spotify Premium", 0,           0,            0,           1   },
   
-  { "Pcmanfm",                    "pcmanfm",                    NULL,              0,           0,            0,           -1  },
+  /* Any Monitor */
+  { "Gpick",                      "gpick",                      NULL,              0,           0,            1,           -1  },
+  { "Nitrogen",                   "nitrogen",                   NULL,              0,           0,            1,           -1  },
+  { "Galculator",                 "galculator",                 NULL,              0,           0,            1,           -1  },
+  { "Pcmanfm",                    "pcmanfm",                    NULL,              0,           0,            1,           -1  },
   { "Pcmanfm",                    "pcmanfm",                    "Execute File",    0,           0,            1,           -1  },
 
 };
@@ -125,13 +132,13 @@ static const Rule rules[] = {
 static const float mfact     = 0.66;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;     /* number of clients in master area */
 static const int resizehints = 1;     /* 1 means respect size hints in tiled resizals */
-static const int decorhints  = 1;     /* 1 means respect decoration hints */
+static const int decorhints  = 0;     /* 1 means respect decoration hints */
 
 /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 static const int attachdirection = 3;
 
 
-static const Layout layouts[] = {
+static const Layout layouts[] = { 
   /* symbol     arrange function */
   { "  ",      col },     /* first entry is default */
   { "舘  ",      tile },    
@@ -169,8 +176,8 @@ static Key keys[] = {
   { ControlMask,          XK_Print,    spawn, CMD("sleep 0.25s; shot") },
   { ShiftMask,            XK_Print,    spawn, CMD("sleep 0.25s; shot focused") },
   { 0,                    XK_Print,    spawn, CMD("sleep 0.25s; shot select") },
+  { MODKEY,               XK_Print,    spawn, CMD("m-scrot") },
   { MODKEY,               XK_l,        spawn, CMD("slock") },
-  { Mod1Mask|ControlMask, XK_Print,    spawn, CMD("m-scrot") },
   { MODKEY,               XK_o,        spawn, CMD("m-quick-cmd") },
   { Mod1Mask|ControlMask, XK_e,        spawn, CMD("m-edit-configs") },
   { Mod1Mask|ControlMask, XK_m,        spawn, CMD("m-sysmon") },
