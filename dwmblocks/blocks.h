dc5ccbde865ebb6fa78e049537548f1d7a44bb5a
abbsi <nabil.abbas@gmail.com>
2020-12-27 11:06:40 +0400

    [dwmblocks] minor edits

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -2,13 +2,13 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon   Command             Update Interval   Update Signal*/
-  {" 阮 ",  "spotify-status",  10,               1},
-  {"  ",  "pacupdate",       360,              9},
-  {"  ",  "memory",          6,                1},
+  {"\x08 阮 ",  "spotify-status",  10,               1},
+  {"\x09   ",  "pacupdate",       360,              9},
+  {"\x0A  ",  "memory",          6,                1},
   {" ﴖ ",  "diskf",           90,               1},
-  {"   ",  "volume",          2,                0},
-  {" 羽 ",  "upt",             60,               2},
-  {"  ",  "clock",           5,                0},
+  {"\x0B   ",  "volume",          2,                0},
+  {"\x0C 羽 ",  "upt",             60,               2},
+  {"\x0D  ",  "clock",           5,                0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
