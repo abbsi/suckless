71035e7ba0ee70ed03d35e23beaa7825d23a82c2
abbsi <nabil.abbas@gmail.com>
2020-12-29 10:57:23 +0400

    [st] selection colors patch applied

--- st/config.def.h
+++ st/config.def.h
@@ -123,6 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"#2e3440",
 	"black",
 };
 
@@ -134,7 +135,15 @@ static const char *colorname[] = {
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
 static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
+
+/* Colors used for selection */
+unsigned int selectionbg = 257;
+unsigned int selectionfg = 7;
+/* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
+/* Else if 1 keep original foreground-color of each cell => more colors :) */
+static int ignoreselfg = 1;
+
 unsigned int bg = 17, bgUnfocused = 16;
 
 /*
