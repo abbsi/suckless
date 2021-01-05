a9a001a2fa828eba35d76c54c35a32128a4b9030
abbsi <nabil.abbas@gmail.com>
2021-01-05 16:57:43 +0400

    [dwm] modified IPC command quit to allow restart through dwm-msg. e.g. `dwm-msg run_command quit 1`

--- dwm/config.h
+++ dwm/config.h
@@ -301,5 +301,5 @@ static IPCCommand ipccommands[] = {
   IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
   IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
   IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
-  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
+  IPCCOMMAND(  quit,                1,      {ARG_TYPE_SINT}   )
 };
